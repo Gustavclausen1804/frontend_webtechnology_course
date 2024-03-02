c9142058f48ca4f869d20c58d80f8c27f6fccb32
Torben Storm Rasmusssen <113907153+tsrDTU@users.noreply.github.com>
2024-03-02 17:36:18 +0100

    Rabat på total pris implementeret

--- src/showTotalPrice.tsx
+++ src/showTotalPrice.tsx
@@ -7,24 +7,47 @@ type ShowTotalPriceProps = {
   const ShowTotalPrice: React.FC<ShowTotalPriceProps> = ({ totalPrice }) => {
     useState(totalPrice);
  
+    function totalPrisMedRabat() : number {
+      let rabat = 0;
+      if (totalPrice > 300) {
+        rabat = totalPrice * 0.1;
+      }
+      return totalPrice - rabat;
+    }
 
+    if (totalPrice > 300) {
+      
     return (
 
-        <div className="frameTot">
-       
-      
-      <table>
-      <tbody>
-      <div className="total-sum">
-      
-                <span>Pris i alt: {totalPrice} DKK</span> {/* Antag at priserne er i DKK */}
-            </div>
-            </tbody>
-            </table>
-      </div>
+          <div className="frameTot">
+         
+        
+        <table>
+        <tbody>
+        <div className="total-sum">
+        
+                  <span>Ialt købes for: {totalPrice} DKK. Afregningspris med 10% rabat: {totalPrisMedRabat().toString()} DKK.</span> {/* Antag at priserne er i DKK */}
+              </div>
+              </tbody>
+              </table>
+        </div>
       
  
     );
+    }
+    else {
+      return (
+        <div className="frameTot">
+          <table>
+          <tbody>
+          <div className="total-sum">
+                  <span>Ialt købes for: {totalPrice} DKK. Der gives 10% rabat ved et samlet køb på over 300 DKK.</span> {/* Antag at priserne er i DKK */}
+              </div>
+              </tbody>
+              </table>
+        </div>
+      );
+    } 
     
   };
             
