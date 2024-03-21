35df91c45c62ffbbf64e63f51337ce5f06dee48e
Torben Storm Rasmusssen <113907153+tsrDTU@users.noreply.github.com>
2024-03-21 22:31:48 +0100

    Kontakt til Backend etableret

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-  
-
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import Payment from './pages/Payment'
-import Receipt from './pages/Receipt'
-
-
-
-
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Payment from './pages/Payment';
+import Receipt from './pages/Receipt';
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element ={<Cart/>}></Route>
-          <Route path="/cart" element = {<Cart/>} />
-          <Route path="/checkout" element = {<Checkout/>} />
-          <Route path="/payment" element = {<Payment/>} />
-          <Route path="/receipt" element = {<Receipt/>} />
-        </Routes>
-      </BrowserRouter>
-    </> 
-  )
+    const [response, setResponse] = useState<string>("");
+
+    async function fetchApi() {
+        try {
+            const response = await fetch('http://130.225.170.52:10331/api');
+            if (!response.ok) {
+                throw new Error(`HTTP fejl: status ${response.status}`);
+            }
+            const data = await response.text();
+            setResponse(data); // Opdater tilstanden med data hentet fra API'en
+        } catch (error) {
+            console.error("Der opstod en fejl under hentning af data: ", error);
+        }
+    }
+
+    return (
+        <>
+            <button onClick={fetchApi}>Call API</button>
+            {response && <p>{response}</p>}
+
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Cart />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/payment" element={<Payment />} />
+                    <Route path="/receipt" element={<Receipt />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
