007a61a4055ac73f16793c086b0537037347bde3
Torben Storm Rasmusssen <113907153+tsrDTU@users.noreply.github.com>
2024-04-04 20:07:19 +0200

    React komponeneter er flyttet op

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,55 @@ import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
 import Payment from './pages/Payment'
 import Receipt from './pages/Receipt'
+import { Product } from './types';
+import React, { useState, useEffect } from 'react';
+/*
+type ProductsComponentProps = {
+    products: Product[];
+  };
+  */
 
 
-
-
+  const [products, setProducts] = useState<Product[]>([]);
 
 function App() {
+    
+    
+  //  const ParentComponent: React.FC = () => {
+    
+  
+    useEffect(() => {
+      async function fetchApi() {
+        try {
+          const response = await fetch('http://dtu62597.eduhost.dk:10331/api');
+          if (!response.ok) {
+            throw new Error(`HTTP error: status ${response.status}`);
+          }
+          const data = await response.json();
+          setProducts(data); // Update the state with data fetched from the API
+        } catch (error) {
+          console.error("An error occurred while fetching data: ", error);
+        }
+      }
+  
+      fetchApi();
+    }, []);
+ /*   
+  return (
+    <div>
+      <ProductsComponent products={products} />
+      {
+    </div>
+  );
+  */
+ // return <Cart products={products} />;
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element ={<Cart/>}></Route>
-          <Route path="/cart" element = {<Cart/>} />
+        <Route index element={<Cart products={products} />} />
+          <Route path="/cart" element = {<Cart products={products}/>} />
           <Route path="/checkout" element = {<Checkout/>} />
           <Route path="/payment" element = {<Payment/>} />
           <Route path="/receipt" element = {<Receipt/>} />
@@ -27,4 +64,6 @@ function App() {
   )
 }
 
-export default App
+
+//export default App
+
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,11 +1,17 @@
 import Header from '../components/Header'
 
-import { products } from '../products'
+//import { products } from '../products'
 import { Product, CartItem } from '../types'
 import CartList from '../CartList'
 
 
-export default function Cart() {
+type CartProps = {
+    products: Product[];
+  };
+  
+  const Cart: React.FC<CartProps> = ({ products }) => {
+
+//export default function Cart(products: Product[]) {
     const sampleCartItems : CartItem[] = products.slice(0, 11).map((product: Product) => ({
         product: product,
         quantity: 1,
@@ -22,3 +28,5 @@ export default function Cart() {
         </>
     )
 }
+
+export default Cart;
