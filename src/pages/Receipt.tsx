1614edda85d317ec20ee872e8d7bbe2c03607291
Torben Storm Rasmusssen <113907153+tsrDTU@users.noreply.github.com>
2024-04-11 22:02:25 +0200

    Receipt resat.

--- src/pages/Receipt.tsx
+++ src/pages/Receipt.tsx
@@ -1,38 +1,9 @@
 import { Link } from 'react-router-dom'
 import Header from '../components/Header'
-import CheckoutForm from '../components/CheckoutForm'
 
- 
 
-async function postData(url = '', data = {}) {
-    // Defaultindstillingerne er indstillet til en POST-anmodning
-    
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // ingen 'cors', 'same-origin'
-      cache: 'no-cache', // *default, ingen-cache, genindlæs, force-cache, only-if-cached
-      credentials: 'same-origin', // inkluder, *same-origin, udeluk
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *følg, fejl
-      referrerPolicy: 'no-referrer', // ingen-referrer, *client
-      body: JSON.stringify(data) // body data type skal matche "Content-Type" headeren
-    });
-    
 
-  
-  function postkommentar() {
-  postData('http://dtu62597.eduhost.dk:10331/api', CheckoutForm)
-    .then(data => {
-      console.log(data); // JSON data, der blev returneret af serveren
-    });
-
-    
-  }
-
-  export default function Receipt() {
+export default function Receipt() {
     return (
         <>
             <Header/>
