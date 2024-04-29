cc1698b596343885fe609006efd7da413b78c879
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2024-04-29 22:07:29 +0200

    Faster page speed: Lazy loading of components not needed on first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import "./styles/App.css"
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-  
-
 import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import Payment from './pages/Payment'
-import Receipt from './pages/Receipt'
 import { ShoppingProvider } from './Context/appContext'
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
+import React from "react";
 
 
 
+const Checkout = React.lazy(() => import('./pages/Checkout'));
+const Payment = React.lazy(() => import('./pages/Payment'));
+const Receipt = React.lazy(() => import('./pages/Receipt'));
   
 
 function App() { 
-  useEffect(() => {
+  useEffect(() => { 
     const loadingElement = document.querySelector('p');
     if (loadingElement) {
       loadingElement.style.display = 'none';
@@ -27,12 +25,24 @@ function App() {
       <ShoppingProvider>
       <BrowserRouter>
         <Routes>
-        
-        <Route index element={<Cart />} />       
+          <Route index element={  <Cart />} />       
           <Route path="/cart" element = {<Cart  />} />
-          <Route path="/checkout" element = {<Checkout/>} />
-          <Route path="/payment" element = {<Payment/>} />
-          <Route path="/receipt" element = {<Receipt/>} />
+          
+          <Route path="/checkout" element={
+            <Suspense fallback={<div>Indlæser checkout siden...</div>}>
+              <Checkout />
+            </Suspense>
+          } />
+          <Route path="/payment" element={
+            <Suspense fallback={<div>Indlæser betalings siden...</div>}>
+              <Payment />
+            </Suspense>
+          } />
+          <Route path="/receipt" element={
+            <Suspense fallback={<div>Indlæser kvitterings siden...</div>}>
+              <Receipt />
+            </Suspense>
+          } />
         </Routes>
       </BrowserRouter>
       </ShoppingProvider>
